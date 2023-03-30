b77f6253e20af3ec976174a8577829eb618e4726
Danny Chen <zhuodannychen@gmail.com>
2023-03-30 11:37:11 -0500

    made changes according to comments

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/interfaces/IERC165.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/interfaces/IERC721.sol";
-import "./IERC4907.sol";
+import "./IRevPass.sol";
 
 contract Marketplace is ReentrancyGuard {
     using Counters for Counters.Counter;
@@ -15,7 +15,6 @@ contract Marketplace is ReentrancyGuard {
 
     Counters.Counter private _numRevPassListed;
     address private _marketOwner;
-    uint256 private _listingFee = .0001 ether; // cost to list a RevPass onto the marketplace
 
     // If multiple collections: mapping(address => mapping(uint256 => Listing)) private _listingMap;
     // maps token id to properties of the rental listing. Assuming there's only the RevPass collection. 
@@ -26,6 +25,8 @@ contract Marketplace is ReentrancyGuard {
     struct Listing {
         address owner; // address of token owner
         address user; // address of renter or zero if none
+        uint256 ownerUIN;
+        uint256 userUIN;
         address nftContract; // address of contract/collection
         uint256 tokenId; // tokenID of the listed NFT within the NFT collection
         uint256 priceToRent; // cost to rent the NFT. Prob a constant since all RevPasses are the same
@@ -36,6 +37,8 @@ contract Marketplace is ReentrancyGuard {
     event NFTListed(
         address owner,
         address user,
+        uint256 ownerUIN,
+        uint256 userUIN,
         address nftContract,
         uint256 tokenId,
         uint256 priceToRent,
@@ -46,12 +49,14 @@ contract Marketplace is ReentrancyGuard {
     event NFTRented(
         address owner,
         address user,
+        uint256 ownerUIN,
+        uint256 userUIN,
         address nftContract,
         uint256 tokenId,
+        uint256 rentalFee, // cost to rent NFT
         uint256 startDateUNIX,
         uint256 endDateUNIX,
-        uint64 expires,
-        uint256 rentalFee // cost to rent NFT
+        uint64 expires
     );
     event NFTUnlisted(
         address unlistSender,
@@ -66,25 +71,72 @@ contract Marketplace is ReentrancyGuard {
     }
 
     // function to list NFT for rental
+    function privateListNFT(
+        address nftContract,
+        uint256 tokenId,
+        uint256 startDateUNIX,
+        uint256 endDateUNIX,
+        uint256 expires,
+        address user,
+        uint256 userUIN,
+        uint256 ownerUIN
+    ) public payable nonReentrant {
+        require(isRentableNFT(nftContract), "Contract is not an ERC4907");
+        require(IERC721(nftContract).ownerOf(tokenId) == msg.sender, "Not owner of nft");
+        require(startDateUNIX >= block.timestamp, "Start date cannot be in the past");
+        require(endDateUNIX >= startDateUNIX, "End date cannot be before the start date");
+        require(_listingMap[tokenId].nftContract == address(0), "This NFT has already been listed");
+
+        _listingMap[tokenId] = Listing(
+            msg.sender,
+            user,
+            ownerUIN,
+            userUIN,
+            nftContract,
+            tokenId,
+            0,
+            startDateUNIX,
+            endDateUNIX,
+            expires
+        );
+
+        _numRevPassListed.increment();
+        EnumerableSet.add(_nftTokensListed, tokenId);
+        emit NFTListed(
+            IERC721(nftContract).ownerOf(tokenId),
+            user,
+            ownerUIN,
+            userUIN,
+            nftContract,
+            tokenId,
+            0,
+            startDateUNIX,
+            endDateUNIX,
+            expires
+        );
+    }
+
+        // function to list NFT for rental
     function listNFT(
         address nftContract,
         uint256 tokenId,
         uint256 priceToRent,
         uint256 startDateUNIX,
-        uint256 endDateUNIX
+        uint256 endDateUNIX,
+        uint256 ownerUIN
     ) public payable nonReentrant {
         require(isRentableNFT(nftContract), "Contract is not an ERC4907");
         require(IERC721(nftContract).ownerOf(tokenId) == msg.sender, "Not owner of nft");
-        require(msg.value == _listingFee, "Not enough ether for listing fee");
         require(priceToRent > 0, "Rental price should be greater than 0");
         require(startDateUNIX >= block.timestamp, "Start date cannot be in the past");
         require(endDateUNIX >= startDateUNIX, "End date cannot be before the start date");
         require(_listingMap[tokenId].nftContract == address(0), "This NFT has already been listed");
 
-        payable(_marketOwner).transfer(_listingFee);
         _listingMap[tokenId] = Listing(
             msg.sender,
             address(0),
+            ownerUIN,
+            0,
             nftContract,
             tokenId,
             priceToRent,
@@ -98,6 +150,8 @@ contract Marketplace is ReentrancyGuard {
         emit NFTListed(
             IERC721(nftContract).ownerOf(tokenId),
             address(0),
+            ownerUIN,
+            0,
             nftContract,
             tokenId,
             priceToRent,
@@ -111,7 +165,8 @@ contract Marketplace is ReentrancyGuard {
     function rentNFT(
         address nftContract,
         uint256 tokenId,
-        uint64 expires
+        uint64 expires,
+        uint256 userUIN
     ) public payable nonReentrant {
         Listing storage listing = _listingMap[tokenId];
         require(listing.user == address(0), "NFT already rented");
@@ -121,20 +176,21 @@ contract Marketplace is ReentrancyGuard {
         require(msg.value >= rentalFee, "Not enough ether to cover rental period");
         payable(listing.owner).transfer(rentalFee);
         // Update listing
-        IERC4907(nftContract).setUser(tokenId, msg.sender, expires);
+        IRevPass(nftContract).setUser(tokenId, msg.sender, expires, userUIN);
         listing.user = msg.sender;
         listing.expires = expires;
 
-        // TODO: Maybe take the RevPass off the marketplace after it has been rented? 
         emit NFTRented(
             IERC721(nftContract).ownerOf(tokenId),
             msg.sender,
+            IRevPass(nftContract).getUserUIN(tokenId),
+            userUIN,
             nftContract,
             tokenId,
+            rentalFee,
             listing.startDateUNIX,
             listing.endDateUNIX,
-            expires,
-            rentalFee
+            expires
         );
     }
 
@@ -143,13 +199,14 @@ contract Marketplace is ReentrancyGuard {
         Listing storage listing = _listingMap[tokenId];
         require(listing.owner != address(0), "This NFT is not listed");
         require(listing.owner == msg.sender || _marketOwner == msg.sender , "Not approved to unlist NFT");
+        require(listing.user == address(0), "This NFT is not rented");
         // fee to be returned to user if unlisted before rental period is up
         uint256 refund = listing.priceToRent;
         // need to write some tests. I think priceToRent is 0 if rent period expires. Or handle case where listing.user is address(0)
         require(msg.value >= refund, "Not enough ether to cover refund");
         payable(listing.user).transfer(refund);
         // clean up data
-        IERC4907(nftContract).setUser(tokenId, address(0), 0);
+        IRevPass(nftContract).setUser(tokenId, address(0), 0, 0);
         EnumerableSet.remove(_nftTokensListed, tokenId);
         delete _listingMap[tokenId];
         _numRevPassListed.decrement();
@@ -185,7 +242,7 @@ contract Marketplace is ReentrancyGuard {
     function isRentableNFT(address nftContract) public view returns (bool) {
         bool _isRentable = false;
         bool _isNFT = false;
-        try IERC165(nftContract).supportsInterface(type(IERC4907).interfaceId) returns (bool rentable) {
+        try IERC165(nftContract).supportsInterface(type(IRevPass).interfaceId) returns (bool rentable) {
             _isRentable = rentable;
         } catch {
             return false;
